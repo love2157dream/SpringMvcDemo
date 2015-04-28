f4a6661bc1b38cd64768bab69613e7c9551d4f9f
love2157dream <2390779141@qq.com>
2015-04-28 17:13:31 +0800

    新建项目

---
+++ src/main/java/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.example.helloworld;
+
+/**
+ * Created by lianglei on 2015/4/27.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World 快捷键！");
+    }
+}
